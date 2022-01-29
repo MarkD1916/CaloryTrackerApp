9df3f67cb9540934dcba38d90012b13197b03e9c
MarkD1916 <vmakd1916@gmail.com>
2022-01-29 17:30:43 +0300

    shared pref setup

--- core/src/main/java/com/vmakd1916gmail/com/core/data/preferences/DefaultPreferences.kt
+++ core/src/main/java/com/vmakd1916gmail/com/core/data/preferences/DefaultPreferences.kt
@@ -11,42 +11,82 @@ class DefaultPreferences(
     private val sharedPref: SharedPreferences
 ) : Preferences {
     override fun saveGender(gender: Gender) {
-        sharedPref.edit().putString(gender.name)
-    }
-
-    override fun saveAge(age: Int) {
-        TODO("Not yet implemented")
-    }
-
-    override fun saveWeight(weight: Float) {
-        TODO("Not yet implemented")
-    }
-
-    override fun saveHeight(height: Int) {
-        TODO("Not yet implemented")
+        sharedPref.edit()
+            .putString(Preferences.KEY_GENDER, gender.name)
+            .apply()
     }
 
     override fun saveActivityLevel(level: ActivityLevel) {
-        TODO("Not yet implemented")
+        sharedPref.edit()
+            .putString(Preferences.KEY_ACTIVITY_LEVEL, level.name)
+            .apply()
     }
 
     override fun saveGoalType(type: GoalType) {
-        TODO("Not yet implemented")
+        sharedPref.edit()
+            .putString(Preferences.KEY_GOAL_TYPE, type.name)
+            .apply()
+    }
+
+    override fun saveAge(age: Int) {
+        sharedPref.edit()
+            .putInt(Preferences.KEY_AGE, age)
+            .apply()
+    }
+
+    override fun saveHeight(height: Int) {
+        sharedPref.edit()
+            .putInt(Preferences.KEY_HEIGHT, height)
+            .apply()
+    }
+
+    override fun saveWeight(weight: Float) {
+        sharedPref.edit()
+            .putFloat(Preferences.KEY_WEIGHT, weight)
+            .apply()
     }
 
     override fun saveCarbRatio(ratio: Float) {
-        TODO("Not yet implemented")
+        sharedPref.edit()
+            .putFloat(Preferences.KEY_CARB_RATIO, ratio)
+            .apply()
     }
 
     override fun saveProteinRatio(ratio: Float) {
-        TODO("Not yet implemented")
+        sharedPref.edit()
+            .putFloat(Preferences.KEY_PROTEIN_RATIO, ratio)
+            .apply()
     }
 
     override fun saveFatRatio(ratio: Float) {
-        TODO("Not yet implemented")
+        sharedPref.edit()
+            .putFloat(Preferences.KEY_FAT_RATIO, ratio)
+            .apply()
     }
 
     override fun loadUserInfo(): UserInfo {
-        TODO("Not yet implemented")
+        val gender = sharedPref.getString(Preferences.KEY_GENDER, null)
+        val activityLevel = sharedPref.getString(Preferences.KEY_ACTIVITY_LEVEL, null)
+        val goalType = sharedPref.getString(Preferences.KEY_GOAL_TYPE, null)
+
+        val age = sharedPref.getInt(Preferences.KEY_AGE, -1)
+        val height = sharedPref.getInt(Preferences.KEY_HEIGHT, -1)
+
+        val weight = sharedPref.getFloat(Preferences.KEY_WEIGHT, -1f)
+        val carbRatio = sharedPref.getFloat(Preferences.KEY_CARB_RATIO, -1f)
+        val proteinRatio = sharedPref.getFloat(Preferences.KEY_PROTEIN_RATIO, -1f)
+        val fatRatio = sharedPref.getFloat(Preferences.KEY_FAT_RATIO, -1f)
+
+        return UserInfo(
+            gender = Gender.fromString(gender ?: "male"),
+            age = age,
+            height = height,
+            weight = weight,
+            carbRatio = carbRatio,
+            proteinRatio = proteinRatio,
+            fatRatio = fatRatio,
+            activityLevel = ActivityLevel.fromString(activityLevel ?: "medium"),
+            goalType = GoalType.fromString(goalType ?: "keep_weight")
+        )
     }
 }
--- core/src/main/java/com/vmakd1916gmail/com/core/domain/model/UserInfo.kt
+++ core/src/main/java/com/vmakd1916gmail/com/core/domain/model/UserInfo.kt
@@ -4,10 +4,10 @@ data class UserInfo(
     val gender: Gender,
     val age: Int,
     val weight: Float,
-    val height: Float,
+    val height: Int,
     val activityLevel: ActivityLevel,
     val goalType: GoalType,
-    val carpRatio: Float,
+    val carbRatio: Float,
     val proteinRatio: Float,
     val fatRatio: Float
 )
