e36450a3148d992b259f2b500879e989e0ad112e
MarkD1916 <vmakd1916@gmail.com>
2022-03-08 17:32:34 +0300

    +ActivityLevel

---
+++ onboarding/onboarding_presentation/src/main/java/com/vmakd1916gmail/com/onbording_presentation/activity_level/ActivityLevelViewModel.kt
@@ -0,0 +1,39 @@
+package com.vmakd1916gmail.com.onbording_presentation.activity_level
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.vmakd1916gmail.com.core.data.preferences.DefaultPreferences
+import com.vmakd1916gmail.com.core.domain.model.ActivityLevel
+import com.vmakd1916gmail.com.core.navigation.Route
+import com.vmakd1916gmail.com.core.util.UiEvent
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class ActivityLevelViewModel @Inject constructor(
+    private val preferences: DefaultPreferences
+) : ViewModel() {
+
+    var selectedActivityLevel by mutableStateOf<ActivityLevel>(ActivityLevel.Medium)
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onActivityClick(activityLevel: ActivityLevel){
+        selectedActivityLevel = activityLevel
+    }
+
+    fun onNextClick(){
+        viewModelScope.launch {
+            preferences.saveActivityLevel(selectedActivityLevel)
+            _uiEvent.send(UiEvent.Navigate(Route.AGE))
+        }
+    }
+}
---
+++ onboarding/onboarding_presentation/src/main/java/com/vmakd1916gmail/com/onbording_presentation/height/HeightViewModel.kt
@@ -0,0 +1,53 @@
+package com.vmakd1916gmail.com.onbording_presentation.height
+
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.vmakd1916gmail.com.core.data.preferences.DefaultPreferences
+import com.vmakd1916gmail.com.core.navigation.Route
+import com.vmakd1916gmail.com.core.use_case.FilterOutDigits
+import com.vmakd1916gmail.com.core.util.UiEvent
+import com.vmakd1916gmail.com.core.util.UiText
+import com.vmakd1916gmail.com.onbording_presentation.R
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.flow.receiveAsFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class HeightViewModel @Inject constructor(
+    private val preferences: DefaultPreferences,
+    private val filterOutDigits: FilterOutDigits
+) : ViewModel() {
+
+    var height by mutableStateOf("175")
+        private set
+
+    private val _uiEvent = Channel<UiEvent>()
+    val uiEvent = _uiEvent.receiveAsFlow()
+
+    fun onHeightEnter(height: String) {
+        if (height.length <= 3) {
+            this.height = filterOutDigits.invoke(height)
+        }
+    }
+
+    fun onNextClick() {
+        viewModelScope.launch {
+            val heightNumber = height.toIntOrNull() ?: kotlin.run {
+                _uiEvent.send(
+                    UiEvent.ShowSnackBar(
+                        UiText.StringResource(R.string.error_height_cant_be_empty)
+                    )
+                )
+                return@launch
+            }
+            preferences.saveHeight(heightNumber)
+            _uiEvent.send(UiEvent.Navigate(Route.ACTIVITY))
+        }
+    }
+}
