c8a690673f3f5e7d97c95d3e7db7dd1e20322c92
MarkD1916 <vmakd1916@gmail.com>
2022-04-11 00:34:04 +0300

    + TrackedFoodItem UI

--- tracker/tracker_presentation/src/main/java/com/vmakd1916gmail/com/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
+++ tracker/tracker_presentation/src/main/java/com/vmakd1916gmail/com/tracker_presentation/tracker_overview/TrackerOverviewScreen.kt
@@ -6,12 +6,16 @@ import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
 import androidx.hilt.navigation.compose.hiltViewModel
 import com.vmakd1916gmail.com.core.util.UiEvent
 import com.vmakd1916gmail.com.core_ui.LocalSpacing
+import com.vmakd1916gmail.com.tracker_presentation.R
 import com.vmakd1916gmail.com.tracker_presentation.components.ExpandableMeal
+import com.vmakd1916gmail.com.tracker_presentation.tracker_overview.components.AddButton
 import com.vmakd1916gmail.com.tracker_presentation.tracker_overview.components.DaySelector
 import com.vmakd1916gmail.com.tracker_presentation.tracker_overview.components.NutrientsHeader
+import com.vmakd1916gmail.com.tracker_presentation.tracker_overview.components.TrackedFoodItem
 import java.nio.file.WatchEvent
 
 @Composable
@@ -43,14 +47,41 @@ fun TrackerOverviewScreen(
             )
             Spacer(modifier = Modifier.height(spacing.spaceMedium))
         }
-        items(state.meals) {meal ->
+        items(state.meals) { meal ->
             ExpandableMeal(
                 meal = meal,
                 onToggleClick = {
-                                viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
+                    viewModel.onEvent(TrackerOverviewEvent.OnToggleMealClick(meal))
                 },
                 content = {
-
+                    Column(
+                        modifier = Modifier
+                            .fillMaxWidth()
+                            .padding(horizontal = spacing.spaceSmall)
+                    ) {
+                        state.trackedFood.forEach { food ->
+                            TrackedFoodItem(trackedFood = food, onDeleteClick = {
+                                viewModel.onEvent(
+                                    TrackerOverviewEvent
+                                        .OnDeleteTrackedFoodClick(food)
+                                )
+                            }
+                            )
+                            Spacer(modifier = Modifier.height(spacing.spaceMedium))
+                        }
+                        AddButton(
+                            text = stringResource(
+                                id = R.string.add_meal,
+                                meal.name.asString(context)
+                            ),
+                            onClick = {
+                                viewModel.onEvent(
+                                    TrackerOverviewEvent.OnAddFoodClick(meal)
+                                )
+                            },
+                            modifier = Modifier.fillMaxWidth()
+                        )
+                    }
                 },
                 modifier = Modifier.fillMaxWidth()
             )
